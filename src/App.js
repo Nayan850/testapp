a3429ee4a164f429c16b257c4b145454730bdcb2
Nayan Sarker <38081322+Nayan850@users.noreply.github.com>
2025-07-15 21:41:42 +0600

    Update3 App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey! babe this is you janu!
+          hello perameter|| 
         </p>
         <a
           className="App-link"
